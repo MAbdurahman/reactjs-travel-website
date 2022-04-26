6fe6e3d941b6c5b4792e0e8c6349ff261c761a71
MAbdurahman <mdbdrrhm@gmail.com>
2022-04-26 09:55:12 -0500

    Complete basic skeleton for react-routes and begin Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+export default function Header() {
+	return (
+		<div>
+			<h2>header component</h2>
+		</div>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
+import './css/main.css';
 import App from './app/App'
 
 
