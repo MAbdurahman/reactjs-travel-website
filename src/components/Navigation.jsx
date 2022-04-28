0cd272ebfb842c447b9d88dd61ac5b108e0b81aa
MAbdurahman <mdbdrrhm@gmail.com>
2022-04-28 07:33:24 -0500

    Style links in Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,6 +6,19 @@ export default function Navigation() {
 	return (
 		<>
 			<div className='nav__layer'></div>
+			<div className='nav'>
+				<div className='nav__content'>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+					<li>
+						<Link to='/contact'>Contact</Link>
+					</li>
+				</div>
+			</div>
 		</>
 	);
 }
