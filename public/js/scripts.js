0174399a1b2e67c244bfc442af93f35959ef478b
MAbdurahman <mdbdrrhm@gmail.com>
2022-11-29 02:09:35 -0600

    Remove extraneous code in ./js/scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,46 +4,6 @@
 $(window).on('load', function () {
 	// makes sure that whole site is loaded
 	$('#preloader--gif, #preloader').fadeOut(3000, function () {});
-	// $('#preloader--gif, #preloader').remove();
-	// console.log('where is preloader?')
-	// $('#preloader').removeAttr('style').hide();
+	
 });
 
-/* $(function () {
-	$('.header__contents__text__child__h1').style.display = 'none';
-}); */
-
-/* =========================================
-            Simple-Scroll-Spy
-============================================ */
-/* window.onload = function () {
-	scrollSpy('#navbar-links-list', {
-		sectionClass: '.scrollspy',
-		menuActiveTarget: '.navbar-links-link',
-		offset: 100,
-	});
-}; */
-
-/* =========================================
-            Progress Bars
-============================================ */
-
-/* $(function () {
-	$('#progress-elements').waypoint(
-		function () {
-			$('.progress-bar').each(function () {
-				$(this).animate(
-					{
-						width: $(this).attr('aria-valuenow') + '%',
-					},
-					1500
-				);
-			});
-
-			this.destroy();
-		},
-		{
-			offset: '100%',
-		}
-	);
-}); */
